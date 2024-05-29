beb6bde978dbf8b4a0efd4932b8a2d7d6566aa21
AlwaysMori <116044642+AlwaysMori@users.noreply.github.com>
2024-05-29 09:04:46 +0700

    adding analyze activity

--- app/src/main/java/com/capstone/hicare/view/splash/SplashActivity.kt
+++ app/src/main/java/com/capstone/hicare/view/splash/SplashActivity.kt
@@ -1,16 +1,12 @@
-package com.capstone.hicare.view
+package com.capstone.hicare.view.splash
 
 import android.annotation.SuppressLint
 import android.content.Intent
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.capstone.hicare.R
 import com.capstone.hicare.view.main.MainActivity
 
