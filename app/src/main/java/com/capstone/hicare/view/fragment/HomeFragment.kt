62aa5ef56182e8df35585a52d6f829c8f07fc9c5
AlwaysMori <116044642+AlwaysMori@users.noreply.github.com>
2024-05-30 12:38:37 +0700

    Disease Lettuce

--- app/src/main/java/com/capstone/hicare/view/fragment/HomeFragment.kt
+++ app/src/main/java/com/capstone/hicare/view/fragment/HomeFragment.kt
@@ -5,11 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.StringRes
 import androidx.recyclerview.widget.GridLayoutManager
+import com.capstone.hicare.R
 import com.capstone.hicare.adapter.DiseaseAdapter
 import com.capstone.hicare.databinding.FragmentHomeBinding
 import com.capstone.hicare.model.Disease
 import com.capstone.hicare.model.factory.DiseaseList.Diseases
+import com.capstone.hicare.model.factory.DiseaseList.addDisease
 import com.capstone.hicare.view.detail.DetailActivity.Companion.EXTRA_DISEASE_DETAIL
 import com.capstone.hicare.view.detail.DetailActivity.Companion.EXTRA_DISEASE_IMAGE
 import com.capstone.hicare.view.detail.DetailActivity.Companion.EXTRA_DISEASE_NAME
@@ -54,12 +57,14 @@ class HomeFragment : Fragment() {
         binding.apply {
             rvListDiseases.layoutManager = GridLayoutManager(activity, 1)
 
-            rvListDiseases.adapter = diseaseAdapter
+            diseaseAdapter= DiseaseAdapter(Diseases)
+            rvListDiseases.adapter=diseaseAdapter
 
             diseaseAdapter.notifyDataSetChanged()
             rvListDiseases.setHasFixedSize(true)
 
-            diseaseAdapter.setOnItemClickCallback(object : DiseaseAdapter.OnItemClickCallBack {
+            diseaseAdapter.setOnItemClickCallback(object :
+                DiseaseAdapter.OnItemClickCallBack{
                 override fun onItemClicked(data: Disease) {
                     selectedDisease(data)
                 }
@@ -76,9 +81,28 @@ class HomeFragment : Fragment() {
     }
 
 
-        override fun onDestroyView() {
-        super.onDestroyView()
+    override fun onDestroy() {
+        super.onDestroy()
         _binding = null
+    }
+
+    override fun onStop() {
+        super.onStop()
+        addDisease(resources).removeAll(Diseases.toSet())
+    }
+
+    override fun onResume() {
+        super.onResume()
+        addDisease(resources)
+
+    }
+
+    companion object {
+        @StringRes
+        private val TAB_TITLES = intArrayOf(
+            R.string.tab_text_1,
+            R.string.tab_text_2
+        )
     }
 }
 
