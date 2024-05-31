d48b2b7bd59ffb27cd3748fe79e248414aeaca54
AlwaysMori <116044642+AlwaysMori@users.noreply.github.com>
2024-05-31 14:29:47 +0700

    updating ui

--- app/src/main/java/com/capstone/hicare/view/setting/SettingActivity.kt
+++ app/src/main/java/com/capstone/hicare/view/setting/SettingActivity.kt
@@ -3,11 +3,14 @@ package com.capstone.hicare.view.setting
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import com.capstone.hicare.R
 
 
 class SettingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        setContentView(R.layout.activity_setting)
     }
+
 }
